3a4c6f88880136b1efd4461f200f569b95476a5a
lourdesreguera <maria.lourdes@bootcamp-upgrade.com>
2022-10-01 16:23:45 +0200

    STYLE: home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,18 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
-    <div>
-      <Link to='/beers'>Pulsa para ver la carta de cervezas</Link>
+    <div className="b-home">
+      <h1 className="b-home__heading">¿Te apetece una buena cerveza y no sabes cuál elegir?</h1>
+      <p className="b-home__subheading">
+        Te ofrecemos una amplia selección de cervezas para que elijas la que más
+        te apetezca hoy. Puedes filtrar por tipo, procedencia y graduación
+        alcohólica. Tras probarla, podrás puntuarla y así ayudar al resto de personas con su elección.
+      </p>
+      <Link className="btn-home" to="/beers">Carta de cervezas</Link>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
